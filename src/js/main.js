57780832dbe7930d35398a4b37ddc31ce3056be6
rafaScalet <rafaelscalet2018@gmail.com>
2024-07-29 14:41:49 -0300

    closes #9 - cadastro de dados

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,27 @@
+const students = [];
+
+const shifts = [
+	'Diurno',
+	'Vespertino',
+	'Noturno',
+];
+
+const courses = [
+	'Java',
+	'.NET',
+	'Javascript',
+];
+
 function save () {
-	console.log('bunda1');
+	const student = {
+		id: students.length + 1,
+		nome: document.getElementById('inputName').value,
+		email: document.getElementById('inputName').value,
+		course: parseInt(document.getElementById('selectCourses').value),
+		shift: parseInt(document.querySelector('input[name="flexRadioShift"]:checked').value),
+	}
+
+	students.push(student);
+	document.getElementById('formStudent').reset();
+	console.log(students[students.length - 1]);
 }
