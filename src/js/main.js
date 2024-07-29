ad439b80773816048aa606c518d9b5eac345bfe7
rafaScalet <rafaelscalet2018@gmail.com>
2024-07-29 15:13:17 -0300

    closes #5 - tabela carregada

--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,29 @@ function save () {
 	}
 
 	students.push(student);
+
+	addNewRow(student);
+
 	document.getElementById('formStudent').reset();
+
 	console.log(students[students.length - 1]);
-}
+};
+
+function addNewRow (student) {
+	const table = document.getElementById('tableStudent');
+	const newRow = table.insertRow();
+
+	const idNode = document.createTextNode(student.id);
+	const nameNode = document.createTextNode(student.name);
+	const emailNode = document.createTextNode(student.email);
+	const phoneNode = document.createTextNode(student.phone);
+	const courseNode = document.createTextNode(courses[student.course]);
+	const shiftNode = document.createTextNode(shifts[student.shift]);
+
+	newRow.insertCell().appendChild(idNode);
+	newRow.insertCell().appendChild(nameNode);
+	newRow.insertCell().appendChild(emailNode);
+	newRow.insertCell().appendChild(phoneNode);
+	newRow.insertCell().appendChild(courseNode);
+	newRow.insertCell().appendChild(shiftNode);
+};
